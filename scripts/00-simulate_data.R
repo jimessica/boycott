89be47474dfe5cbe54520c7b34e11426f3025400
Jessica <jessicajwooim@gmail.com>
2024-03-28 09:47:39 -0400

    updated simulate preamble

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,17 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates stock prices of 5 boycotted companies in the United States from 1980 to 2020
+# Author: Jessica Im
+# Date: 28 March 2024
+# Contact: jessica.im@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(ggplot2)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
 
 
 
