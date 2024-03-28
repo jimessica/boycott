3bf2e859dab8543dc084d99de18eb2daa072fbd1
Jessica <jessicajwooim@gmail.com>
2024-03-28 10:26:53 -0400

    create simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -14,10 +14,24 @@ library(dplyr)
 #### Simulate data ####
 set.seed(250)
 
+# simulate data of company 1 from 1 year before to 1 year after boycott
 KO_simulated_data <-
   tibble(
-    year = 
-  )
+    date =
+      seq(as.Date('2002-06-01'), as.Date('2006-05-01'), by = "months"),
+    
+    # size is 48 to represent the number of months in total between 1 year before boycott began and 1 year after it ended
+    high = 
+      sample(
+        rep(c(16:32)),
+        size = 48,
+        replace = TRUE),
+    
+    volume =
+      sample(
+        rep(c(1000000:99999999)),
+        size = 48,
+        replace = TRUE),
+      )
 
-
-
+KO_simulated_data
